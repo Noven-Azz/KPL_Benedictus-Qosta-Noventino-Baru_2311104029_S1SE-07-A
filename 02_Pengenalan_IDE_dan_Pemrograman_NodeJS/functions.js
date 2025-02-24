40081ced61a6a4e59fd507d429be932cc0512651
Benedictus Noven <novenbaru@gmail.com>
2025-02-24 12:06:06 +0700

    pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/functions.js
@@ -0,0 +1,17 @@
+function perkalian(a, b) {
+  let result = a * b;
+  console.log(result);
+}
+
+const pertambahan = (a, b) => {
+  let result = a + b;
+  console.log(result);
+};
+
+const pembagian = (a, b) => {
+  let result = a / b;
+  console.log(result);
+};
+
+console.log(pekalian(2, 3));
+console.log(pertambahan(2, 3));
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variables.js
@@ -0,0 +1,15 @@
+var name = "Benedictus Noven";
+const nim = 2311104029;
+
+console.log(name);
+
+console.log("Nama saya adalah" + name + " dan nim saya yaitu " + nim);
+
+console.log(`Nama saya adalah ${name} dan nim saya yaitu ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya yaitu ${nim + 1}`);
+
+var name = "ayam";
+
+let fullName = "Benedictus Noven";
